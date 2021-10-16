6cc1adef57d984a7d57d6b1bbbbd9f0fea056594
Diana DiPalma <diana.dipalma@alaskaair.com>
2021-10-15 17:31:41 -0700

    accept command line arguments and only clone repos if they don't contain a codeowners file

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
 # add-codeowners-github
 
-To run this script, ``source ./addCodowners.sh``
-
-
-It will create a new subdirectory & will clone fresh repos owned by the team/org combo provided, create a new branch, add the codeowners file, & post a PR.
+It will create a new subdirectory & will clone fresh repos owned by the team/org combo provided (this is only executed if a CODEOWNERS file does not exist), create a new branch, add the codeowners file, & post a PR.
 
 The end of the script will print a list of open PRs.
+
+## Prerequisites
+
+To run this script, you will need to install the github cli & jq.
+
+Installation instructions for the github cli can be found here:
+https://github.com/cli/cli
+
+We use the github cli to create the PR and we are assuming that the git cli is already installed on your machine (please install it if it is not)! :)
+
+Installation instructions for jq can be found here:
+https://stedolan.github.io/jq/
+
+JQ is a lightweight and flexible command-line JSON processor that we use to parse the response from the github APIs.
+
+## Running The Script
+Execute the following line (replace the variables with your personal/team specific information and make sure your arguments are wrapped in quotes), ``source ./addCodowners.sh -p '$PAT' -u '$USERNAME' -t '$TEAM' -o '$ORG' -a '$AB'``
+
+All of the following arguments are required:
+ - -p takes your PAT (you will need to make sure you have authorized access to the Alaska-ECommerce org in GitHub)
+ - -u takes the username you use for github
+ - -t takes your team name (do not include the '@')
+ - -o takes the org name (this is 'Alaska-ECommerce' in most cases)
+ - -a takes the azure work item (only the numbers)
--- addCodeowners.sh
+++ addCodeowners.sh
@@ -1,13 +1,49 @@
-brew install gh
-brew install git-lfs
-brew install ruby
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -pat ghPersonalAccessToken -t team-name -u user-name -o org -ab azureWorkItemNumber"
+   echo -e "\t-pat provide a personal access token from your github account"
+   echo -e "\t-t The github team name, such as 'flight-search' (do not include the '@')"
+   echo -e "\t-u Your github username (do not include the '@')"
+   echo -e "\t-o The github org where your team's repos can be found (such as, 'Alaska-ECommerce')"
+   echo -e "\t-ab The work item number to be added to your PR for AB Validation (such as '12345')"
+   exit 1 # Exit script after printing help
+}
 
-PAT=""
-TEAM="flight-search"
-USERNAME="diana-dipalma"
-ORG="Alaska-ECommerce"
-AB="11111"
+while getopts p:t:u:o:a: opt
+do
+   case "$opt" in
+      p ) PAT="$OPTARG" ;;
+      t ) TEAM="$OPTARG" ;;
+      u ) USERNAME="$OPTARG" ;;
+      o ) ORG="$OPTARG" ;;
+      a ) AB="$OPTARG" ;; 
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
 
+echo $PAT $TEAM $USERNAME $ORG $AB
+
+if [ -z "$PAT" ]; then
+    echo "FAILURE: Missing PAT (personal access token)"
+    exit 2;
+fi
+if [ -z "$TEAM" ]; then
+    echo "FAILURE: Missing Team"
+    exit 2;
+fi
+if [ -z "$USERNAME" ]; then
+    echo "FAILURE: Missing Username"
+    exit 2;
+fi
+if [ -z "$ORG" ]; then
+    echo "FAILURE: Missing ORG"
+    exit 2;
+fi
+if [ -z "$AB" ]; then
+    echo "FAILURE: Missing AB (Azure Story Board number)"
+    exit 2;
+fi
 
 mkdir "addCodeowners"
 cd "./addCodeowners"
@@ -19,8 +55,21 @@ gh auth login --with-token < pat.txt
 
 echo "Cloning Team Repos"
 
-#ruby sucks (python map method)
-$(curl -u $TEAM:$PAT -s https://api.github.com/orgs/$ORG/teams/$TEAM/repos | ruby -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git lfs clone #{repo["clone_url"]} ]}')
+repos=$(curl -u $TEAM:$PAT -s https://api.github.com/orgs/$ORG/teams/$TEAM/repos | sed 's/ //g')
+
+for repo in $(echo "${repos}" | jq -c -r '.[]'); do
+   name=$(echo $repo | jq -r '.name')
+   url=$(echo $repo | jq -r '.clone_url')
+   respCode1=$(curl --write-out "%{http_code}\n" --silent --output /dev/null -u $TEAM:$PAT -s https://api.github.com/repos/$ORG/$name/contents/CODEOWNERS)
+   respCode2=$(curl --write-out "%{http_code}\n" --silent --output /dev/null -u $TEAM:$PAT -s https://api.github.com/repos/$ORG/$name/contents/docs/CODEOWNERS)
+   respCode3=$(curl --write-out "%{http_code}\n" --silent --output /dev/null -u $TEAM:$PAT -s https://api.github.com/repos/$ORG/$name/contents/.github/CODEOWNERS)
+  if [ $respCode1 = "200" ] || [ $respCode2 = "200" ] || [ $respCode3 = "200" ]; then
+    echo "$name already contains a CODEOWNERS file"
+  else
+    git clone $url
+  fi
+   
+done
 
 echo "Repos cloned. Starting to add CODEOWNERS"
 
@@ -29,30 +78,22 @@ pr_list=()
 for dir in ./* ; do
   if [ -d "$dir" ]; then
     dir=${dir%*/}
-
-    echo $dir
-    if [ $dir != "./AlaskaAirCom" ] && [ $dir != "./AsComDeployScripts" ]; then
-      mkdir -p ${dir##*/}/.github
-      echo "# Default owners of everything (i.e. *) in the repository \n* @$ORG/$TEAM" > ./${dir##*/}/.github/CODEOWNERS
-
-      echo "Updating $dir repo"
-      cd ${dir##*/}
-      git checkout -b $USERNAME/CODEOWNERS
-      git add .
-      git commit -m "Added CODEOWNERS file"
-      git push origin $USERNAME/CODEOWNERS
-      pr=$(gh pr create --title "Adding CODEOWNERS" --body "AB#$AB" --reviewer @$ORG/$TEAM 2>&1)
-      echo $pr
-      pr_list+=( "$pr" )
-      cd ..
-      echo "codeowners pushed to branch, $USERNAME/CODEOWNERS"
-    fi
+    mkdir -p ${dir##*/}/.github
+    echo "# Default owners of everything (i.e. *) in the repository \n* @$ORG/$TEAM" > ./${dir##*/}/.github/CODEOWNERS
+    echo "Updating $dir repo"
+    cd ${dir##*/}
+    git checkout -b $USERNAME/CODEOWNERS
+    git add .
+    git commit -m "Added CODEOWNERS file"
+    git push origin $USERNAME/CODEOWNERS
+    pr=$(gh pr create --title "Adding CODEOWNERS" --body "AB#$AB" --reviewer "@$ORG/$TEAM" 2>&1)
+    echo $pr
+    pr_list+=( "$pr" )
+    cd ..
+    echo "codeowners pushed to branch, $USERNAME/CODEOWNERS"
   fi
 done
 
-echo "${pr_list[*]}"
+cd ..
 
-# pass in parameters
-# check if codeowners already exist (root, .github, docs) > check if URL is valid
-
-
+printf '%b\n' "${pr_list[@]}"
